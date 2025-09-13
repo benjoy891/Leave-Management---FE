8780fe6a8575a91c729a57e76dd681e2c78fb337
Ben Joy <benjoybj891@gmail.com>
2025-09-13 17:14:01 +0530

    Rebuild web with new logo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2729422513"
+    serviceWorkerVersion: "4285177467"
   }
 });
